0b174c01422e7adaed653890b03f537b3d0d85e6
Gerson Santos <gersones@outlook.com>
2021-04-17 09:41:33 -0300

    aa

--- aluraflix/app.js
+++ aluraflix/app.js
@@ -1,11 +1,8 @@
 function adicionarFilme()
 {
-    console.log("Clicou")
-
     console.log("funcionou javascritp");
     var campoFilme=document.querySelector('#filme')
     var filmeFavorito=campoFilme.value
-    console.log(filmeFavorito)
     if (filmeFavorito.endsWith('.jpg'))
     {
      listarFilmesNaTela(filmeFavorito) 
